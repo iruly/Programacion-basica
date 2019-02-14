5896ff0178be1326eeb9a3b6f5f1eef92ed5e067
iruly <layes47@hotmail.com>
2019-02-14 16:45:18 -0500

    mi nuevo dibujo

---
+++ canvas/dibujo.js
@@ -0,0 +1,18 @@
+
+var d = document.getElementById("dibujito");
+
+var lienzo = d.getContext("2d");
+
+lienzo.beginPath();
+lienzo.strokeStyle= "blue";
+lienzo.moveTo(50, 10);
+lienzo.lineTo(250,2);
+lienzo.stroke();
+lienzo.closePath();
+
+lienzo.beginPath();
+lienzo.strokeStyle= "red";
+lienzo.moveTo(100, 10);
+lienzo.lineTo(290,200);
+lienzo.stroke();
+lienzo.closePath();
---
+++ codigo.js
@@ -0,0 +1,6 @@
+
+var ruta = window.location;
+
+    console.log(ruta);
+
+document.write("Estas en:" + ruta.origin);
