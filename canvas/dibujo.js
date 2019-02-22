1925f377904c6c4196b76d4a7369c0a408659610
iruly <layes47@hotmail.com>
2019-02-22 09:41:38 -0500

    agregando codigo para carpeta peso

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -1,18 +1,30 @@
 
 var d = document.getElementById("dibujito");
-
 var lienzo = d.getContext("2d");
+var lineas=30;
+var l= 0;
+var yi,xf;
+var colorcito = "#AFA";
 
-lienzo.beginPath();
-lienzo.strokeStyle= "blue";
-lienzo.moveTo(50, 10);
-lienzo.lineTo(250,2);
-lienzo.stroke();
-lienzo.closePath();
+for ( l= 0; l < lineas; l++)
 
-lienzo.beginPath();
-lienzo.strokeStyle= "red";
-lienzo.moveTo(100, 10);
-lienzo.lineTo(290,200);
-lienzo.stroke();
-lienzo.closePath();
+{
+  yi = 10 +l;
+  xf=  10 * (l+ 1);
+  dibujarLinea(colorcito, 0,yi, xf,300);
+  console.log("lineas" + l);
+}
+dibujarLinea(colorcito, 1,1,1,299);
+dibujarLinea(colorcito, 1,299,299,299);
+
+
+
+function dibujarLinea(color, x_inicial, y_inicial, x_final, y_final)
+{
+ lienzo.beginPath();
+ lienzo.strokeStyle= color;
+ lienzo.moveTo(x_inicial, y_inicial);
+ lienzo.lineTo(x_final, y_final);
+ lienzo.stroke();
+ lienzo.closePath();
+}
